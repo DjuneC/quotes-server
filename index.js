1f79b2ba19033e29053a96bc5921944c03ceca59
Rodelin J Cesar <cesarrodelin@outlook.com>
2024-09-12 21:24:41 -0500

    Refactor #001

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 import express from "express";
 
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
 import quotes from "./data.js";
 
 const app = express();
 
 app.set("view engine", "ejs");
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const PORT = process.env.PORT || 4000;
 
 
 app.get("/", (req, res) => {
-    res.send(`<div>
-                <h3>${quotes[0].quote}</h3>
-                <h5>${quotes[0].author}</h5>
-            </div>`);
+    res.render("index", { quotes })
 })
 
 app.listen(PORT, () => {
