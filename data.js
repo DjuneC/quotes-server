3f5dfcfdb439296178e693620319154f4af063bb
Rodelin J Cesar <cesarrodelin@outlook.com>
2024-09-12 19:40:14 -0500

    First functional code that sends back the quote and his related author to the client

--- data.js
+++ data.js
@@ -3,31 +3,31 @@ const quotes = [
       id: 1,
       author: "Unknown",
       quote: "Success is not for the lazy. Push through the doubt, the fear, and the failures. Keep moving forward, one step at a time.",
-      category: "Motivation"
+      category: "motivation"
     },
     {
       id: 2,
       author: "Gabrielle Bernstein",
       quote: "The Universe is always responding to your energy. Your thoughts, beliefs, and feelings are creating your reality.",
-      category: "Manifestation"
+      category: "manifestation"
     },
     {
       id: 3,
       author: "Leo Buscaglia",
       quote: "Love is life. And if you miss love, you miss life.",
-      category: "Love"
+      category: "love"
     },
     {
       id: 4,
       author: "Jon Kabat-Zinn",
       quote: "Mindfulness is the awareness that arises from paying attention, on purpose, in the present moment and non-judgmentally.",
-      category: "Mindful"
+      category: "mindful"
     },
     {
       id: 5,
       author: "Unknown",
       quote: "Your vibration is a reflection of your inner state. Elevate your thoughts, and your life will rise with them.",
-      category: "Vibration"
+      category: "vibration"
     }
   ];
 
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from "express";
 
+import quotes from "./data.js";
+
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+
 app.get("/", (req, res) => {
-    res.send("My personal quotes!");
-    
+    res.send(`<div>
+                <h3>${quotes[0].quote}</h3>
+                <h5>${quotes[0].author}</h5>
+            </div>`);
 })
 
 app.listen(PORT, () => {
