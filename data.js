f1e99670a29a2a58c644a64ca8dcc840584c7199
Rodelin J Cesar <cesarrodelin@outlook.com>
2024-09-12 19:27:29 -0500

    Add sample data for testing purposes | Will use postgres in the future

---
+++ data.js
@@ -0,0 +1,35 @@
+const quotes = [
+    {
+      id: 1,
+      author: "Unknown",
+      quote: "Success is not for the lazy. Push through the doubt, the fear, and the failures. Keep moving forward, one step at a time.",
+      category: "Motivation"
+    },
+    {
+      id: 2,
+      author: "Gabrielle Bernstein",
+      quote: "The Universe is always responding to your energy. Your thoughts, beliefs, and feelings are creating your reality.",
+      category: "Manifestation"
+    },
+    {
+      id: 3,
+      author: "Leo Buscaglia",
+      quote: "Love is life. And if you miss love, you miss life.",
+      category: "Love"
+    },
+    {
+      id: 4,
+      author: "Jon Kabat-Zinn",
+      quote: "Mindfulness is the awareness that arises from paying attention, on purpose, in the present moment and non-judgmentally.",
+      category: "Mindful"
+    },
+    {
+      id: 5,
+      author: "Unknown",
+      quote: "Your vibration is a reflection of your inner state. Elevate your thoughts, and your life will rise with them.",
+      category: "Vibration"
+    }
+  ];
+
+  export default quotes;
+  
